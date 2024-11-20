5afb7383b744db33a59cc58cad246232418aab13
Park EunSol <eunsolguni1353@gmail.com>
2024-11-20 10:01:40 +0900

    two

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 public static void main(String[] args) {
-// 자신의 학번과 이름을 출력하라
+// 자신의 학번과 이름을 입력하라
 System.out.println("202114047 박은솔");
+System.out.println(Hello.sum(10));
+System.out.println(Hello.sum(100));
 }
 }
