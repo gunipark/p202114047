64fe3d9720b33aa8bf74343b4cbf59c9d7d708d0
Park EunSol <eunsolguni1353@gmail.com>
2024-11-20 10:10:02 +0900

    five

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
 public static void main(String[] args) {
 // 자신의 학번과 이름을 입력하라
-System.out.println("20211447 박은솔");
+System.out.println("202114047 박은솔");
 System.out.println(Hello.sum(10));
 System.out.println(Hello.sum(100));
 System.out.println(Hello.sum(1000));
+System.out.println(Sansu.max(new int[] {8, 5, 6, 9, 4, 1, 7, 10, 3, 2, 8}));
+System.out.println(Sansu.max(new int[] {-2, 5, 6, 9, -6, 1, 7, 2, 8, 10}));
 }
 }
